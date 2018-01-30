180fc054bb1c7f82b0bbe3ba8193fa48587a4ef1
Sean Oughton <seanoughton@gmail.com>
2018-01-30 18:28:54 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,13 @@ def monopoly_with_third_tier
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
-    },
+    }
+		:names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      }
 		}
 	}
 
