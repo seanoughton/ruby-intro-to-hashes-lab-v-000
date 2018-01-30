b71b89c2502edf2098f0f08de41a6ce4586ff3e4
Sean Oughton <seanoughton@gmail.com>
2018-01-30 18:43:18 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ def monopoly_with_fourth_tier
 			:pieces=>4,
 			:names => {
 				:reading_railroad=>{"mortgage_value"=>"$100"},
-				:pennsylvania_railroad=>{mortgage_value"=>"$200"},
+				:pennsylvania_railroad=>{"mortgage_value"=>"$200"},
 				:b_and_o_railroad =>{"mortgage_value"=>"$400"},
 				:shortline=>{"mortgage_value"=>"$800"}
 			},
